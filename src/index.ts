1244284e525c87938a2cf3f47e698ceec67f0acb
ramah-madi <ramahmadi2001@gmail.com>
2026-05-18 18:02:48 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response):void => {res.send('Hello from Express server');});
+
+app.listen(port, ():void => {
+    console.log(`Listening on port http://localhost:${port}`);
+})
