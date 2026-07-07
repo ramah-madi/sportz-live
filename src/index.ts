e35b32137c3dd54ff7d6ad3abf1f87ba53188311
ramah-madi <ramahmadi2001@gmail.com>
2026-07-07 21:50:21 +0300

    feat: integrate APM Insight monitoring and initialize express server in main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express, { Request, Response } from "express";
 import http from "http";
 import matcheRouter from "./routes/matches.js";
